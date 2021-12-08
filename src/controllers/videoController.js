ec09bd4eaa6bb89feaf7bc6900f9f7607be81528
spongebob03 <bmofinnjake08@gmail.com>
2021-12-08 17:21:28 +0900

    #5.3 template

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,5 @@
-export const trending = (req, res) => res.send("Trending video");
-export const watch = (req, res) => {
-    console.log(`Watch Vidoe #${req.params.id}`);
-    return res.send(`Watch Vidoe #${req.params.id}`);
-};
+export const trending = (req, res) => res.render("home");
+export const watch = (req, res) => res.render("watch");
 export const upload = (req, res) => res.send("Upload");
 export const edit = (req, res) => res.send("Edit");
 export const deleteVideo = (req, res) => res.send("Delete Video");
