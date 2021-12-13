625c680accac3e7242e87f2c9bf8a4fd5d6fbdc3
spongebob03 <bmofinnjake08@gmail.com>
2021-12-13 17:38:36 +0900

    callback

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,43 +1,21 @@
-let videos = [
-    {
-        title: "First Video",
-        rating: 5, 
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 1,
-        id: 1,
-    },
-    {
-        title: "Second Video",
-        rating: 5, 
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 2,
-    },
-    {
-        title: "Third Video",
-        rating: 5, 
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 3,
-    }
-];
-export const trending = (req, res) => {
-    
-    return res.render("home", {pageTitle: "Home", videos});
+import Video from "../models/Video";
+
+export const home = (req, res) => {
+    console.log("1");
+    Video.find({}, (error, videos) => {
+        console.log("3!");
+        return res.render("home", {pageTitle: "Home", videos});
+    });
+    console.log("2")
 };
 export const watch = (req, res) => {
     const {id} = req.params;
-    const video = videos[id - 1];
-    return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
+    return res.render("watch", {pageTitle: `Watching`});
 };
 export const upload = (req, res) => res.send("Upload");
 export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id -1];
-    return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
+    return res.render("edit", {pageTitle: `Editing`});
 }
 export const postEdit = (req, res) => {
     const { id } = req.params;
@@ -54,14 +32,5 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
     const { title } = req.body;
-    const newVideo = {
-        title,
-        rating: 0,
-        comments: 0,
-        createdAt: "just now",
-        views: 0,
-        id: videos.length + 1,
-    };
-    videos.push(newVideo);
     return res.redirect("/");
 };
