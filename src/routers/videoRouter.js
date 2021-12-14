e21d49924597026a75cedc2da11ad2840919ed6a
spongebob03 <bmofinnjake08@gmail.com>
2021-12-14 18:57:13 +0900

    #6.19 watch-정규표현식

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,8 +2,8 @@ import express from "express";
 import { watch, edit, upload, deleteVideo, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
