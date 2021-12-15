f1379e8b65a883d6ff11fa7e3ae1f596b3c3ad97
spongebob03 <bmofinnjake08@gmail.com>
2021-12-15 19:05:59 +0900

    update post

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -13,21 +13,38 @@ export const home = async(req, res) => {
 export const watch = async (req, res) => {
     const {id} = req.params;
     const video = await Video.findById(id);
-
+    if (!video) {
+        return res.render("404", {pageTitle:"Video not found."});
+    }
     return res.render("watch", {pageTitle: video.title, video });
 };
 
 export const upload = (req, res) => res.send("Upload");
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    return res.render("edit", {pageTitle: `Editing`});
+    const video = await Video.findById(id);
+
+    if (!video) {
+        return res.render("404", {pageTitle: "Video not found."});
+    }
+    return res.render("edit", {pageTitle: `Editing`, video});
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
-    videos[id - 1].title = title;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({ _id: id});
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found."});
+    }
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: hashtags
+            .split(",")
+            .map((word) => (word.startsWith("#")? word: `#${word}`)),
+    });
     return res.redirect(`/videos/${id}`);
 };
 
@@ -45,12 +62,7 @@ export const postUpload = async(req, res) => {
         await Video.create({
             title,
             description,
-            createdAt: Date.now(),
-            hashtags: hashtags.split(",").map((word) => `#${word}`),
-            meta: {
-                views: 0,
-                rating: 0,
-            },
+            hashtags: hashtags.split(",").map((word) => (word.startsWith("#")? word: `#${word}`)),
         });
         return res.redirect("/");
     } catch(error) {
