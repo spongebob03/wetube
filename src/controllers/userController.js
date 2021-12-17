08e816dcdff517a510d79004b277bcbc47e25f64
spongebob03 <bmofinnjake08@gmail.com>
2021-12-17 15:23:21 +0900

    User Join

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,18 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("Join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+    const {name, username, email, password, location} = req.body;
+    await User.create({
+        name, 
+        username,
+        email,
+        password,
+        location,
+    });
+    return res.redirect("/login");
+};
+
 export const login = (req, res) => res.send("LogIn");
 export const logout = (req, res) => res.send("LogOut");
 export const see = (req, res) => res.send("See");
