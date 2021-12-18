5a6a4ccbbabb487d28eb88be4a94838e7b05c8b8
spongebob03 <bmofinnjake08@gmail.com>
2021-12-18 16:08:35 +0900

    Login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("Join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -18,18 +19,45 @@ export const postJoin = async (req, res) => {
             errorMessage: "This username/email is already taken.",
         });
     }
-
-    await User.create({
-        name, 
-        username,
-        email,
-        password,
-        location,
-    });
+    try{
+        await User.create({
+            name, 
+            username,
+            email,
+            password,
+            location,
+        });
     return res.redirect("/login");
+    } catch(error) {
+        return res.status(400).render("join", {
+            pageTitle: "Join",
+            errorMessage: error._message,
+        });
+    }
 };
 
-export const login = (req, res) => res.send("LogIn");
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+    const { username, password } = req.body;
+    const pageTitle = "Login";
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "An account with this username does not exists.",
+        });
+    }
+    // check pw
+    const ok = await bcrypt.compare(password, user.password);
+    if (!ok){
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "Wrong password",
+        });
+    }
+    return res.redirect("/");
+};
+
 export const logout = (req, res) => res.send("LogOut");
 export const see = (req, res) => res.send("See");
 export const edit = (req, res) => res.send("Edit");
