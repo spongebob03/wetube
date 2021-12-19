abac7cd86a837ba4f2d2ff19e1fb5c8e80f408d9
spongebob03 <bmofinnjake08@gmail.com>
2021-12-19 15:40:42 +0900

    login system

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import morgan from "morgan";
-
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
@@ -13,6 +14,15 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger)
 app.use(express.urlencoded({ extended: true}));
+
+app.use(
+    session({
+        secret: "Hello!",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
